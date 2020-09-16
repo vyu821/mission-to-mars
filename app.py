509631f3adf19e6521cadcb0acb68b8a48936c9b
vyu821 <67933865+vyu821@users.noreply.github.com>
2020-09-15 19:40:37 -0700

    added index.html, debugging app.py, flask gives 500 internal server error

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # import dependencies
 from flask import Flask, render_template
-from flask_pymongo import flask_pymongo
+from flask_pymongo import PyMongo
 import scraping
 
 # set up flask app instance
 app = Flask(__name__)
 
-# use flask_pymongo to set up mongo connection
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 # first route
@@ -16,8 +16,11 @@ mongo = PyMongo(app)
 def index():
     # uses pymongo to find 'mars' collection in our database
     mars = mongo.db.mars.find_one()
-    # return html template using index.html file, use 'mars' collection in mongodb
-    return render_template('index.html', mars = mars)
+    try:
+        # return html template using index.html file, use 'mars' collection in mongodb
+        return render_template('index.html', mars = mars)
+    except Exception as e:
+        return str(e)
 
 # scrape route
 @app.route('/scrape')
--- scraping.py
+++ scraping.py
@@ -3,81 +3,125 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 
 import pandas as pd
+import datetime as dt
 
-# Set the executable path and initialize the chrome browser in splinter
-executable_path = {'executable_path': 'chromedriver'}
-browser = Browser('chrome', **executable_path)
+# function that initializes browser
+# create data dictionary
+# end webdriver and return scraped data
+def scrape_all():
 
-# mars nasa news site
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
-# Optional delay for loading the page
-browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    # Set the executable path and initialize the chrome browser in splinter
+    browser = Browser('chrome', executable_path = 'chromedriver', headless = True)
+    # use mars_news function to pull this data
+    news_title, news_paragraph = mars_news(browser)
 
-# html parser
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('ul.item_list li.slide')
+    # data dictionary
+    data = {
+        'news_title': news_title,
+        'news_paragraph': news_paragraph,
+        'featured_image': feature_image(browser),
+        'facts': mars_facts(),
+        'last_modified': dt.datetime.now()
+    }
 
-# find article title
-slide_elem.find("div", class_='content_title')
+    # stop webdriver and return data
+    # end automated browsing session
+    browser.quit()
+    return data
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-# get_text() returns only texst and not any of the html tags or elements
-news_title = slide_elem.find("div", class_='content_title').get_text()
-news_title
+# function that returns news title and paragraph
+def mars_news(browser):
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
-news_p
+    # visit mars nasa news site
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    # Optional delay for loading the page
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
-### Featured Images
+    # convert browser html to soup object, then quit browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    
+    # add try/except for error handling
+    try:
 
-# Visit URL
-url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-browser.visit(url)
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
 
-# Find and click the full image button
-# variable to hold scraping result, browser finds element by its id
-full_image_elem = browser.find_by_id('full_image')
-# splinter will click the image
-full_image_elem.click()
+        # find article title
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        # get_text() returns only texst and not any of the html tags or elements
+        news_title = slide_elem.find("div", class_='content_title').get_text()
 
-# Find the more info button and click that
-# search for an element that has "more info" text
-browser.is_element_present_by_text('more info', wait_time=1)
-# take our string "more info" to find the link associated with the "more info" text
-more_info_elem = browser.links.find_by_partial_text('more info')
-# splinter will click the link
-more_info_elem.click()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    except AttributeError:
+        return None, None
 
-# Find the relative image url
-# look inside <figure class="lede" /> tag for an <a /> tag, then look within that <a /> tag 
-# for an <img /> tag
-img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-img_url_rel
+    return news_title, news_p
 
-# Use the base URL to create an absolute URL
-img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-img_url
+# function that returns the featured image
+def feature_image(browser):
 
-# finds and returns list of tables found in the html, index 0 for the first table
-df = pd.read_html('http://space-facts.com/mars/')[0]
-# assign column names
-df.columns=['description', 'value']
-# turning description column into the df's index, inplace=true means updated index will remain in place
-# without having to reassign df to new variable
-df.set_index('description', inplace=True)
-df
+    # Visit URL
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
 
-# convert df back to html-ready code
-df.to_html()
+    # Find and click the full image button
+    # variable to hold scraping result, browser finds element by its id
+    full_image_elem = browser.find_by_id('full_image')
+    # splinter will click the image
+    full_image_elem.click()
 
-# end automated browsing session
-browser.quit()
+    # Find the more info button and click that
+    # search for an element that has "more info" text
+    browser.is_element_present_by_text('more info', wait_time=1)
+    # take our string "more info" to find the link associated with the "more info" text
+    more_info_elem = browser.links.find_by_partial_text('more info')
+    # splinter will click the link
+    more_info_elem.click()
 
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
+    # try/except for errors
+    try:
+
+        # Find the relative image url
+        # look inside <figure class="lede" /> tag for an <a /> tag, then look within that <a /> tag 
+        # for an <img /> tag
+        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+    
+    except AttributeError:
+        return None
+
+    # Use the base URL to create an absolute URL
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+
+    return img_url
+
+# function that returns mars facts
+def mars_facts():
+
+    # try/except for errors
+    try:
+        # finds and returns list of tables found in the html, index 0 for the first table
+        df = pd.read_html('http://space-facts.com/mars/')[0]
+
+    except BaseException:
+        return None
+    
+    # assign column names
+    df.columns=['description', 'value']
+    # turning description column into the df's index, inplace=true means updated index will remain in place
+    # without having to reassign df to new variable
+    df.set_index('description', inplace=True)
+
+    # convert df back to html-ready code
+    return df.to_html(classes="table table-striped")
+
+# tells Flask our script is complete and ready for action
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
