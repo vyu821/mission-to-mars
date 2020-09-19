89abc6f73fdfc8cccb0befb2956797a51bb85e2b
vyu821 <67933865+vyu821@users.noreply.github.com>
2020-09-18 22:58:33 -0700

    edited scraping.py to include mars hemispheres data

--- scraping.py
+++ scraping.py
@@ -21,7 +21,8 @@ def scrape_all():
         'news_paragraph': news_paragraph,
         'featured_image': feature_image(browser),
         'facts': mars_facts(),
-        'last_modified': dt.datetime.now()
+        'last_modified': dt.datetime.now(),
+        'hemispheres': mars_hemis(browser)
     }
 
     # stop webdriver and return data
@@ -120,6 +121,61 @@ def mars_facts():
 
     # convert df back to html-ready code
     return df.to_html(classes="table table-striped")
+
+# function that returns hemisphere data
+def mars_hemis(browser):
+    
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    # list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # retrieve the image urls and titles for each hemisphere.
+    # Parse the resulting html with soup
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+
+    # holds all the tags that contain links to the images
+    all_pics = hemi_soup.find_all('div', class_='description')
+    # counter to iterate through h3 tags
+    i = 0
+
+    # loops through the tags
+    for pic in all_pics:
+        
+        # empty dictionary to hold title and full-res image URL string of each hemisphere iamge
+        hemispheres = {}
+        # finds all h3 tags, these contain the links
+        element = browser.find_by_tag('h3')
+        # checks to see if browser has loaded
+        if browser.is_element_present_by_tag('h3'):
+            
+            # gets the title of each hemisphere image
+            title = element[i].text
+            # clicks the link to the full-res image
+            element[i].click()
+            # parse the second page containing the full-res image
+            html_2 = browser.html
+            hemi_2_soup = soup(html_2, 'html.parser')
+            
+            # holds all tags that contain the link
+            full_res = hemi_2_soup.find('div', class_='downloads')
+            # gets the full-res image URL
+            image = full_res.find('a')['href']
+            
+            # create dictionay with title and URL
+            hemispheres['img_url'] = image
+            hemispheres['title'] = title
+            # updates list with dictionary
+            hemisphere_image_urls.append(hemispheres)
+            
+        # add one for the next tag    
+        i = i + 1
+        # go back to main page for next image
+        browser.back()
+
+    return hemisphere_image_urls
 
 # tells Flask our script is complete and ready for action
 if __name__ == "__main__":
